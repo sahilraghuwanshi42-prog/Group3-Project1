c634099b2237b5861ed52cad50f3798ea2da0fb7
Vinod Raghuwanshi <sahilraghuwanshi42@gmail.com>
2026-06-12 23:53:13 +0530

    Week 1 completed - document upload system

--- documents/admin.py
+++ documents/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Document
-# Register your models here.
+from .models import Document, ExtractedClause, RiskFlag
+
 admin.site.register(Document)
+admin.site.register(ExtractedClause)
+admin.site.register(RiskFlag)
--- documents/models.py
+++ documents/models.py
@@ -7,3 +7,29 @@ class Document(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ExtractedClause(models.Model):
+    document = models.ForeignKey(
+        Document,
+        on_delete=models.CASCADE,
+        related_name='clauses'
+    )
+    clause_text = models.TextField()
+    clause_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.clause_type
+
+
+class RiskFlag(models.Model):
+    document = models.ForeignKey(
+        Document,
+        on_delete=models.CASCADE,
+        related_name='risk_flags'
+    )
+    risk_text = models.TextField()
+    risk_level = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.risk_level
