7c6ba7fefc12258d6756ab18de092dc02addc147
Vinod Raghuwanshi <sahilraghuwanshi42@gmail.com>
2026-06-21 23:24:54 +0530

    Added text cleaning and page-wise PDF extraction

--- documents/pdf_utils.py
+++ documents/pdf_utils.py
@@ -1,13 +1,30 @@
 import fitz
+import re
 
 def extract_text_from_pdf(pdf_path):
     text = ""
-
     pdf = fitz.open(pdf_path)
-
     for page in pdf:
         text += page.get_text()
+    pdf.close()
+    return text
+
+def clean_text(text):
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip()
+
+
+def extract_pages_from_pdf(pdf_path):
+    pdf = fitz.open(pdf_path)
+
+    pages = []
+
+    for page_num, page in enumerate(pdf, start=1):
+        pages.append({
+            "page_number": page_num,
+            "text": page.get_text()
+        })
 
     pdf.close()
 
-    return text
+    return pages
--- documents/tests.py
+++ documents/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# from documents.pdf_utils import extract_text_from_pdf #, clean_text
+# # text = extract_text_from_pdf("pdfs/Python_Project.pdf")
+# # cleaned_text = clean_text(text)
+# # print(cleaned_text[:1000])
 
-# Create your tests here.
+
+from documents.pdf_utils import extract_pages_from_pdf
+
+pages = extract_pages_from_pdf("pdfs/Python_Project.pdf")
+
+for page in pages:
+    print(f"\n--- Page {page['page_number']} ---")
+    print(page['text'][:500])
