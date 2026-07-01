c5c09682e9a45ba3b5b9a9dc2118b9e80ad8fed7
Vinod Raghuwanshi <sahilraghuwanshi42@gmail.com>
2026-07-01 22:01:32 +0530

    Create nasted serializers for clauses and risk flags

--- documents/serializers.py
+++ documents/serializers.py
@@ -1,7 +1,39 @@
 from rest_framework import serializers
-from . models import Document
+from .models import Document, ExtractedClause, RiskFlag
+
+
+class ExtractedClauseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExtractedClause
+        fields = "__all__"
+
+
+class RiskFlagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RiskFlag
+        fields = "__all__"
+
 
 class DocumentSerializer(serializers.ModelSerializer):
+    clauses = ExtractedClauseSerializer(
+        source="extractedclause_set",
+        many=True,
+        read_only=True
+    )
+
+    risk_flags = RiskFlagSerializer(
+        source="riskflag_set",
+        many=True,
+        read_only=True
+    )
+
     class Meta:
-         model = Document
-         fields = '__all__'
+        model = Document
+        fields = [
+            "id",
+            "title",
+            "pdf_file",
+            "uploaded_at",
+            "clauses",
+            "risk_flags",
+        ]
